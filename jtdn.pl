a3b7c0f45e648409109916d5809ce8e843c6613d
Jon Linnell <linnell.jon@gmail.com>
2014-11-22 23:10:27 +0000

    Added new options (--no-rt, --monitor)

--- jtdn.pl
+++ jtdn.pl
@@ -11,22 +11,52 @@ use Log::Minimal;
 use DBI;
 use Getopt::Long;
 
-$Log::Minimal::AUTODUMP=1;
-$Log::Minimal::COLOR=1;
+$Log::Minimal::AUTODUMP = 1;
+$Log::Minimal::COLOR = 1;
 
-my $usage = "Usage: ".$0." [--monitor] [--config path] --keywords keywords --db path\nSee README for more information.\n\n";
+#######  DATABASE SETTINGS  #######
+my %db_attr =
+(
+	PrintError 			=> 0,
+	RaiseError 			=> 1,
+	mysql_enable_utf8 	=> 1
+);
+
+my $table_seed_sql =
+	"CREATE TABLE tweets
+	(
+		username		VARCHAR(48) not null,
+		fullname		VARCHAR(64),
+		timestamp_ms	BIGINT,
+		tweet_id		VARCHAR(20),
+		text			VARCHAR(160)
+	);";
+
+#	jtdn creates a 160-character field for storing Tweet text, in order not to truncate the text in the case of
+#	a badly interpretted HTML character or something.
+
+###################################
+
+my $usage = "Usage: ".$0."\n
+   [--config path]\t- Specifies OAuth config file.   Default: ./auth.conf
+   --db path\t\t- Specifies output SQLite database. Currently mandatory.
+   --keywords keywords\t- Search keywords.
+   [--monitor]\t\t- Print Tweets to STDOUT in realtime.
+   [--no-rt]\t\t- Omit quote retweets (i.e. any tweet that begins with 'RT @'.)\n\nSee README for more information.\n\n";
 
 my $config_file = 'auth.conf';
 my $keywords;
 my $db_file;
 my $monitor;
+my $no_rt;
 
 GetOptions
 	(
-		"config=s" => \$config_file,
-		"db=s" => \$db_file,
+		"config=s"	=> 	\$config_file,
+		"db=s"		=>	\$db_file,
 		"keywords=s" => \$keywords,
-		"monitor" => \$monitor
+		"monitor"	=> 	\$monitor,
+		"no-rt"		=>	\$no_rt
 	)
 	or die $usage;
 
@@ -35,20 +65,13 @@ die $usage unless defined $db_file;
 
 my $config = do $config_file or die "Can't load $config_file : $!$@";
 
-my $dbh_table =		$config->{dbh_table};
-
 die "You must acquire a Twitter API token before using this program.\n\n" unless defined $config->{token_secret};
-
-# Database things
 
 my $dsn = "DBI:SQLite:dbname=".$db_file;
 
-my %db_attr =
-(
-	PrintError 			=> 0,
-	RaiseError 			=> 1,
-	mysql_enable_utf8 	=> 1
-);
+###################################
+########## END OF CONFIG ##########
+###################################
 
 unless (-e $db_file)
 {
@@ -60,8 +83,6 @@ unless (-e $db_file)
 	close $newdb;
 
 	my $dbh_new = DBI->connect($dsn, "", "", \%db_attr);
-
-	my $table_seed_sql = "CREATE TABLE tweets (username VARCHAR(48) not null, fullname VARCHAR(64), timestamp_ms BIGINT, tweet_id VARCHAR(20), text VARCHAR(140));";
 
 	my $dbquery = $dbh_new->do($table_seed_sql);
 
@@ -80,7 +101,11 @@ unless (-e $db_file)
 my $dbh = DBI->connect($dsn, "", "", \%db_attr);
  
 my $done = AnyEvent->condvar;
- 
+
+###################################
+######### BEGIN LISTENING #########
+###################################
+
 my $listener = AnyEvent::Twitter::Stream->new(
 	token           => $config->{token},
 	token_secret    => $config->{token_secret},
@@ -93,7 +118,12 @@ my $listener = AnyEvent::Twitter::Stream->new(
 	{
 		my $tweet = shift;
 
-		if ( defined $monitor )
+		if (defined $no_rt)
+		{
+			if ($tweet->{text} =~ /^RT @/) { next; }
+		}
+
+		if (defined $monitor)
 		{
 			my $tweet_no_newline = $tweet->{text};
 			my $count = $tweet_no_newline =~ s/\r?\n/ /g;
@@ -107,8 +137,9 @@ my $listener = AnyEvent::Twitter::Stream->new(
 			print "\n\n";
 		}
 
-		my $query_string = "INSERT INTO tweets(username, fullname, timestamp_ms, tweet_id, text)
-			VALUES(?, ?, ?, ?, ?);";
+		my $query_string = 
+			"INSERT INTO tweets(username, fullname, timestamp_ms, tweet_id, text)
+			 VALUES(?, ?, ?, ?, ?);";
 
 		my $query = $dbh->prepare($query_string);
 
@@ -133,5 +164,3 @@ my $listener = AnyEvent::Twitter::Stream->new(
 $done->recv;
 
 $dbh->disconnect();
-
-
